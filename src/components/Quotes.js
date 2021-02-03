a14884ef892b967104b3dddd31d2e6e31a6bc19d
Lukecp <lukecp@pm.me>
2021-02-02 22:44:06 -0500

    MVP Completed

--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -1,21 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
+import { Button } from 'reactstrap';
 
-	//Set up fetch button and event handler
-	//Set up actions, middleware, and API call
+import { getQuote } from '../actions';
 
-const Quotes = (props) => {
-	return (
-		<>
-		<h2>"{props.quote}"</h2>
-		</>
+import './Quotes.css';
+
+
+	const Quotes = (props) => {
+		const [copyButtonText, setCopyButtonText] = useState('Copy to clipboard')
+		
+		const onClickHandler = (e) => {
+			e.preventDefault();
+			props.getQuote();
+		}
+
+		const copyHandler = (e) => {
+			e.preventDefault();
+			var dummy = document.createElement("input");
+			document.body.appendChild(dummy);
+			dummy.setAttribute('value', props.quote);
+			dummy.select();
+			document.execCommand("copy");
+			document.body.removeChild(dummy);
+			setCopyButtonText('Copied!')
+			setTimeout(() => setCopyButtonText('Copy to clipboard'), 2000)
+		}
+
+		return (
+		<div className="quote-container">
+		{ props.isFetching ? <h2>Fetching...</h2>
+		: props.quote      ? <h3>"{props.quote}"</h3>
+		: props.error      ? <h2 style={{color: "red"}}>{props.error}<br></br>Please try again</h2>
+		: ''
+		}
+		<Button color="primary" onClick={onClickHandler}>Get quote</Button>
+		<Button color="primary" onClick={copyHandler}>{copyButtonText}</Button>
+		</div>
 	);
 };
 
 const mapStateToProps = (state) => {
 	return {
-		quote: state.quote
+		quote: state.quote,
+		isFetching: state.isFetching,
+		error: state.error
 	}
 }
 
-export default connect(mapStateToProps)(Quotes);
+export default connect(mapStateToProps, { getQuote })(Quotes);
